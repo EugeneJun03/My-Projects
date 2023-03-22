bb63450fa651fd6cd3942dd7a706814da777a6a5
Eugene Jun <genenamu@gmail.com>
2023-03-22 23:25:04 +0900

    feat: Add fuctions - get_number_of_lines(), main()
    We added max of lines and get a number we gonna gamble

--- gambling_game.py
+++ gambling_game.py
@@ -1,7 +1,9 @@
+MAX_LINE = 3
+
 def deposit():
     while True:
         amount = input("What would you like to diposit? $:")
-        if amount.isdigit():
+        if amount.isdigit(): # is.digit()은 입력받은 값이 숫자인지 판별해 준다.
             amount = int(amount)
             if amount > 0:
                 break
@@ -10,3 +12,24 @@ def deposit():
         else:
             print("Please enter a number.")
     return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on(1-" + str(MAX_LINE) + ")? ")
+        if lines.isdigit(): # is.digit()은 입력받은 값이 숫자인지 판별해 준다.
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINE:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+    return lines
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
