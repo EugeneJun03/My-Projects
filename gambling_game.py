d2894a401e94c66d01afac6fc7d77466fcd06bd3
Eugene Jun <genenamu@gmail.com>
2023-03-23 22:28:26 +0900

    feat: Add some input programs
    Add func get_bet() to get how much do you want to bet
    
    Add loop at main() to go back and get right amount of money

--- gambling_game.py
+++ gambling_game.py
@@ -1,5 +1,6 @@
 MAX_LINE = 3
-
+MAX_BET = 100
+MIN_BET = 1
 def deposit():
     while True:
         amount = input("What would you like to diposit? $:")
@@ -26,10 +27,30 @@ def get_number_of_lines():
             print("Please enter a number.")
     return lines
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $:")
+        if amount.isdigit(): # is.digit()은 입력받은 값이 숫자인지 판별해 준다.
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between {MIN_BET} - {MAX_BET}.")
+        else:
+            print("Please enter a number.")
+    return amount
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is: {balance}")
+        else:
+            break
+    print(f"your are betting {bet} on {lines} lines. Total bet is equal to: {total_bet}")
 
 main()
