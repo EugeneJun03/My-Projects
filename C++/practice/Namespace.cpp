21b4a0be238dd3c1c5718775730c5fe4f783b212
Eugene Jun <genenamu@gmail.com>
2024-10-18 16:38:35 +0200

    Learning about Namespace

---
+++ C++/practice/Namespace.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define TEXT "Welcome to C++ Programming!!"
+using namespace std;// this makes us to easly use std with out using "std::"
+
+// You can add an comment like this
+
+/* when you want to make
+many another comment in
+different line*/
+
+int main()
+{
+    cout << TEXT;
+    return 0;
+}
