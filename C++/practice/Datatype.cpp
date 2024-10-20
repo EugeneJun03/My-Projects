c89179119598267be94ce9e87a9a52bc50dfe0df
Eugene Jun <genenamu@gmail.com>
2024-10-21 00:24:33 +0200

    Make: Datatype.cpp

---
+++ C++/practice/Datatype.cpp
@@ -0,0 +1,23 @@
+/*There are Big 4 data types in C++
+1.int: 1, 2, 3
+2.double: 3.14
+3.char: 'A', 'B'
+4.bool: 'true' or 'false'
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    double height = 1.83;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is student " << isStudent << endl;
+
+    return 0;
+}
