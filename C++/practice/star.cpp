4707479c3eb1047abf4649b5d4ad537e4f88cc94
Eugene Jun <genenamu@gmail.com>
2024-10-21 23:54:08 +0200

    Feat: add comment

--- C++/practice/star.cpp
+++ C++/practice/star.cpp
@@ -3,11 +3,12 @@
 int main() {
     int row= 10;
 
+    // Double for statement
     for (int i = 1; i <= row; i++) {
         for (int j = 1; j <= i; j++) {
             std::cout << "*";
         }
-        std::cout << std::endl;
+        std::cout << std::endl; // change line
     }
     return 0;
 
