edc401495fcb6a5cc58086fd317aa836c9b43050
Eugene Jun <genenamu@gmail.com>
2024-10-21 23:24:36 +0200

    Feat: add Decreasing for statement

--- C++/practice/For.cpp
+++ C++/practice/For.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
 int main() {
+    // Increase
     for (int i = 1; i <= 5; i++) {
         std::cout << "Count: " << i << std::endl;
     }
+
+    std::cout << "---------"<< std::endl;
+    
+    // Decrease
+    for (int i = 5; i >= 1; i--) {  // 조건을 i >= 1로 수정
+        std::cout << "Count: " << i << std::endl;
+    }
+
     return 0;
 }
