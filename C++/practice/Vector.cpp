b41b08063c2009aac8dafe16c4eaee7b2da7143d
Eugene Jun <genenamu@gmail.com>
2024-10-22 13:10:16 +0200

    Feat: add Removeal methods

--- C++/practice/Vector.cpp
+++ C++/practice/Vector.cpp
@@ -25,6 +25,27 @@ int main() {
     for (int i = 0; i < v.size(); i++) {
         cout << "Element " << i << ": " << v.at(i) << endl;  // Access each element by index and print it
     }
+
+    // Remove a specific number from the vector
+    int b;
+    cout << "Give a number that you want to remove in vector(end if -1): " << endl;
+    cin >> b;
+    while (b != -1) {
+        for (auto it = v.begin(); it != v.end(); it++) { //"auto" allows the compiler to determine the type of a variable automatically
+            if (*it == b) { // *it gives you the actual element's value that the iterator points(it) to.
+                v.erase(it); // Erase the element at the iterator's position
+                break;
+            }
+        }
+        cout << "Give a number that you want to remove in vector(end if -1): " << endl;
+        cin >> b;
+    }
     
+    std::cout << "Vector after removal: ";
+    for (int num : v) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
     return 0;  // Return 0 to indicate successful execution
 }
